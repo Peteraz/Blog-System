f7a8f5f68a780ba1a51b04556e543bd6579812a3
PeterKuan <673840304@qq.com>
2021-08-10 19:13:31 +0800

    修改名字

--- src/main/java/com/example/blogsystem/BlogSystemApplication.java
+++ src/main/java/com/example/blogsystem/BlogSystemApplication.java
@@ -1,13 +1,13 @@
-package com.example.firstdemo;
+package com.example.blogsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstDemoApplication {
+public class BlogSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FirstDemoApplication.class, args);
+        SpringApplication.run(BlogSystemApplication.class, args);
     }
 
 }
--- src/test/java/com/example/blogsystem/BlogSystemApplicationTests.java
+++ src/test/java/com/example/blogsystem/BlogSystemApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.firstdemo;
+package com.example.blogsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FirstDemoApplicationTests {
+class BlogSystemApplicationTests {
 
     @Test
     void contextLoads() {
