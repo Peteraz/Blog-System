f3296c8cc066f087c44372101a760466597e5fab
USER <windows9.99999@hotmail.com>
2021-08-17 21:47:58 +0800

    配置mybatis,添加了application,generateConfig等文件,并在pom添加了一些包

---
+++ BlogSystem-entity/src/test/java/com/blogsystem/TestUUID.java
@@ -0,0 +1,10 @@
+package com.blogsystem;
+
+import java.util.UUID;
+
+public class TestUUID {
+    public static void main(String[] args) {
+        String userid= UUID.randomUUID().toString().replace("-", "");
+        System.out.println("UUID:"+userid);
+    }
+}
