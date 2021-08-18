b10e5415e70adf656dc2519d74752d692b4e2295
PeterKuan <673840304@qq.com>
2021-08-18 11:10:46 +0800

    注解修正,generatorConfig.xml补上缺少的属性,使它不会生成其他库的字,重新生成entity,mapper,dao等文件

--- BlogSystem-user-provider/src/main/java/com/example/blogsystemuserprovider/dao/UserMapper.java
+++ BlogSystem-user-provider/src/main/java/com/example/blogsystemuserprovider/dao/UserMapper.java
@@ -1,42 +1,17 @@
 package com.example.blogsystemuserprovider.dao;
 
 import com.example.blogsystem.entity.User;
-import com.example.blogsystem.entity.UserExample;
-import com.example.blogsystem.entity.UserKey;
-import com.example.blogsystem.entity.UserWithBLOBs;
-import java.util.List;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-//@Mapper就是告诉spring这是你指定的mapper
-@Mapper
 public interface UserMapper {
-    long countByExample(UserExample example);
+    int deleteByPrimaryKey(String userid);
 
-    int deleteByExample(UserExample example);
+    int insert(User record);
 
-    int deleteByPrimaryKey(UserKey key);
+    int insertSelective(User record);
 
-    int insert(UserWithBLOBs record);
+    User selectByPrimaryKey(String userid);
 
-    int insertSelective(UserWithBLOBs record);
-
-    List<UserWithBLOBs> selectByExampleWithBLOBs(UserExample example);
-
-    List<User> selectByExample(UserExample example);
-
-    UserWithBLOBs selectByPrimaryKey(UserKey key);
-
-    int updateByExampleSelective(@Param("record") UserWithBLOBs record, @Param("example") UserExample example);
-
-    int updateByExampleWithBLOBs(@Param("record") UserWithBLOBs record, @Param("example") UserExample example);
-
-    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
-
-    int updateByPrimaryKeySelective(UserWithBLOBs record);
-
-    int updateByPrimaryKeyWithBLOBs(UserWithBLOBs record);
+    int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
 }
