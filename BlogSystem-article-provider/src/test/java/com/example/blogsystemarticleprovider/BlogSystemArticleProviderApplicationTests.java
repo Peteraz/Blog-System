549ba8233336c66a128f72905e178c5237c55abd
PeterKuan <673840304@qq.com>
2021-08-20 23:40:49 +0800

    添加Hystrix配置

---
+++ BlogSystem-article-provider/src/test/java/com/example/blogsystemarticleprovider/BlogSystemArticleProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogsystemarticleprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSystemArticleProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- BlogSystem-consumer/src/main/java/com/example/blogsystemconsumer/service/UserProviderService.java
+++ BlogSystem-consumer/src/main/java/com/example/blogsystemconsumer/service/UserProviderService.java
@@ -1,5 +1,6 @@
 package com.example.blogsystemconsumer.service;
 
+import com.example.blogsystemconsumer.Hystrix.HystrixUserClientService;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +12,7 @@ import java.util.Map;
 
 //name 为user-provider项目中application.yml配置文件中的application.name;
 //path 为user-provider项目中application.yml配置文件中的context.path;
-@FeignClient(name = "user-provider-server",path="/user-provider")
+@FeignClient(name = "user-provider-server",path="/user-provider",fallbackFactory = HystrixUserClientService.class)
 //@Component:把普通pojo实例化到spring容器中，相当于配置文件中的 <bean id="" class=""/>,也就是配置类
 //@Componet注解最好加上，不加idea会显示有错误，但是不影响系统运行；
 @Component
