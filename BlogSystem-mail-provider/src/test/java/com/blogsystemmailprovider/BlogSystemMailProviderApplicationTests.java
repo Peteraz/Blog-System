1c9a09466047a3f1f273f05366730405554c8953
PeterKuan <673840304@qq.com>
2021-08-23 00:12:46 +0800

    添加mail-provider项目

---
+++ BlogSystem-mail-provider/src/test/java/com/blogsystemmailprovider/BlogSystemMailProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogsystemmailprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSystemMailProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
