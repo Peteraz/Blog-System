4ccb76bf5fd63a72e1afb0c043ef459073d004b8
PeterKuan <673840304@qq.com>
2021-09-14 15:00:18 +0800

    熔断配置circuitbreaker经过修改后通过测试了,删除了spring-cloud-loadbalancer依赖

--- BlogSystem-product/src/main/java/com/example/blogsystemproduct/controller/ProductController.java
+++ BlogSystem-product/src/main/java/com/example/blogsystemproduct/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.example.blogsystemproduct.controller;
 
 import com.example.blogsystem.entity.Product;
+import org.apache.tomcat.jni.Time;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 //@RestController注解，相当于@Controller+@ResponseBody两个注解的结合，返回json数据不需要在方法前面加@ResponseBody注解了，
 // 但使用@RestController这个注解，就不能返回jsp,html页面，视图解析器无法解析jsp,html页面
@@ -15,6 +18,7 @@ public class ProductController {
     }
     @RequestMapping(value="getService")
     public String getService(){
+        Time.sleep(6000);
         String serverport="这是8001服务提供者,当你看到些字时候就是没问题了！";
         return serverport;
     }
